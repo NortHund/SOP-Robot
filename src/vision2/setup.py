c5a29eb9c91d51d1464ffdce4c189cf06fe952e6
mikael <mikael@VLFBERHT>
2022-06-14 19:56:01 +0300

    cleaned unused nodes, moved explisten to vision2 node, file response.py

--- src/vision2/setup.py
+++ src/vision2/setup.py
@@ -25,7 +25,8 @@ setup(
         'console_scripts': [
             'vision2_node = vision2.vision2_node:main',
             'object_tracker_node = vision2.object_tracker_node:main',
-            'expression_detection_node = vision2.expression_detection_node:main'
+            'expression_detection_node = vision2.expression_detection_node:main',
+            'response = vision2.response:main'
         ],
     },
 )
